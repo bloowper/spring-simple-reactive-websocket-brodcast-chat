0d3e57a421a58c588f57f314db3d52935da6079c
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2022-05-22 12:42:16 +0200

    initial state of chat

---
+++ src/main/java/com/orchowski/seminariumreactivechat/SeminariumReactiveChatApplication.java
@@ -0,0 +1,13 @@
+package com.orchowski.seminariumreactivechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminariumReactiveChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeminariumReactiveChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orchowski/seminariumreactivechat/SeminariumReactiveChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orchowski.seminariumreactivechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeminariumReactiveChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
